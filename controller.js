0c788b9f0e8c2178c5370ee348325b1502951cb9
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-10-17 13:29:21 +0530

    Complete backend

--- controller.js
+++ controller.js
@@ -1,1 +1,23 @@
- 
+ const users = [
+    {
+        id:1,
+        name: 'John',
+    },
+    {
+        id:2,
+        name: 'Prasad',
+    }
+ ];
+
+ const getUsers = (cb) => {
+    cb(users);
+ };
+
+ const getUserById = (id,cb) => {
+    const user = users.find(user => user.id == id);
+    cb(user);
+ };
+
+ exports.getUsers = getUsers;
+
+ exports.getUserById = getUserById;
--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const port = 3001;
 const host = '127.0.0.1';
 
-const server = app.listen(3001, '127.0.0.1', ()=>{
-    console.log("Node Server is Working. ${server.address().port}");
+const server = app.listen(port, host, ()=>{
+    console.log("Node Server is Working.");
 })
