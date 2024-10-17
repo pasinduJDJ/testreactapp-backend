12e3ff79d712b804f0c5e1b547a23a560e509ffd
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-10-17 14:16:53 +0530

    Add Mongodb Atlas

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const app = require("./app");
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+const uri ='mongodb+srv://mngodb:mongodbpwd@notesapp.qdo1q.mongodb.net/?retryWrites=true&w=majority&appName=notesapp';
+
+const connect  = async ()=>{
+    try{
+        await mongoose.connect(uri);
+        console.log('mongodb Connected');
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+connect();
 
 const server = app.listen(port, host, ()=>{
     console.log("Node Server is Working.");
