c7753d05da21d6fba236e2a9f90dc15ca61f13d8
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-10-17 17:02:49 +0530

    Crud methods Complete

--- controller.js
+++ controller.js
@@ -1,23 +1,55 @@
- const users = [
-    {
-        id:1,
-        name: 'John',
-    },
-    {
-        id:2,
-        name: 'Prasad',
-    }
- ];
+const User = require('./model');
 
- const getUsers = (cb) => {
-    cb(users);
+const getUsers = (req, res, next) => {
+   User.find()
+      .then(response => {
+         res.json({ response })
+      })
+      .catch(error => {
+         res.json({ error })
+      })
+};
+
+const addUser = (req, res, next) => {
+   const user = new User({
+      id: req.body.id,
+      name: req.body.name,
+   });
+   user.save()
+      .then(response => {
+         res.json({ response })
+      })
+      .catch(error => {
+          res.json({error })
+      })
  };
 
- const getUserById = (id,cb) => {
-    const user = users.find(user => user.id == id);
-    cb(user);
+ const updateUser = (req, res, next) => {
+   const id = req.body.id;
+   const name = req.body.name;
+
+   User.updateOne({id:id},{$set:{name:name}})
+      .then(response => {
+          res.json({ response })
+      })
+      .catch(error => {
+         res.json({error })
+      })
  };
 
- exports.getUsers = getUsers;
+ const deleteUser = (req,res,next) =>{
+   const id = req.body.id;
+   
+   User.deleteOne({id:id})
+      .then(response => {
+         res.json({ response })
+      })
+      .catch(error => {
+          res.json({error })
+      })
+ }
 
- exports.getUserById = getUserById;
+exports.getUsers = getUsers;
+exports.addUser = addUser;
+exports.deleteUser = deleteUser;
+exports.updateUser = updateUser;
